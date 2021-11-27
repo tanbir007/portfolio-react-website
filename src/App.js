7abe751ea90bcc935f402d80d4011bf75e19bee9
tanbir007 <tanbir.1121@.com>
2021-11-27 19:13:07 +0600

    portfolio-first-commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+
+import './App.css';
+import About from './components/about/about';
+import Intro from './components/intro/intro';
+import Profile from './components/profile/Profile';
+
+
+function App() {
+  return (
+    
+<div>
+  <Intro/>
+  <About/>
+  
+
+
+</div>
+    
+  );
+}
+
+export default App;
---
+++ src/components/profile/Profile.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+        <div>
+            <h3>ksjfhdksjbhdfjksbjfjhdfbjh0154</h3>
+        </div>
+    );
+};
+
+export default Profile;
